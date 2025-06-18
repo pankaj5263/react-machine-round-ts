1eb1f5e13ae91a779f52637d2350c7fece0fc8d9
Pankaj Sharma <pankaj.sharma@pankajsharmass-493.successivetech.com>
2025-06-18 15:23:45 +0530

    scss setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({ open: true }) // opens analysis in browser
+    visualizer({ open: true })
   ],
 });
